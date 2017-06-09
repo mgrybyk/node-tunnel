2c7f6726d05e1ae056888abe73940f79c35d7bb2
Mykola Grybyk <mgrybyk@serena.com>
2017-06-09 03:40:24 +0300

    proxy: removed possible memory leak

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -8,51 +8,50 @@ const info = console.log.bind(console)
 const HOST = process.env.N_T_PROXY_HOST || '127.0.0.1'
 const PORT = process.env.N_T_PROXY_PORT || '8888'
 const server = socks.createServer(function (socket, port, address, proxy_ready) {
-      // Implement your own proxy here! Do encryption, tunnelling, whatever! Go flippin' mental!
-      // I plan to tunnel everything including SSH over an HTTP tunnel. For now, though, here is the plain proxy:
+  // Implement your own proxy here! Do encryption, tunnelling, whatever! Go flippin' mental!
+  // I plan to tunnel everything including SSH over an HTTP tunnel. For now, though, here is the plain proxy:
 
-  var proxy = net.createConnection({port: port, host: address, localAddress: process.argv[2] || undefined}, proxy_ready)
+  var proxy = net.createConnection({ port: port, host: address, localAddress: process.argv[2] || undefined }, proxy_ready)
   var localAddress, localPort
   proxy.on('connect', function () {
     info('%s:%d <== %s:%d ==> %s:%d', socket.remoteAddress, socket.remotePort,
-              proxy.localAddress, proxy.localPort, proxy.remoteAddress, proxy.remotePort)
+      proxy.localAddress, proxy.localPort, proxy.remoteAddress, proxy.remotePort)
     localAddress = proxy.localAddress
     localPort = proxy.localPort
   })
+  socket.on('drain', function () {
+    if (proxy.isPaused()) proxy.resume()
+  })
   proxy.on('data', function (d) {
     try {
-          // console.log('receiving ' + d.length + ' bytes from proxy');
+      // console.log('receiving ' + d.length + ' bytes from proxy');
       if (!socket.write(d)) {
         proxy.pause()
 
-        socket.on('drain', function () {
-          proxy.resume()
-        })
         setTimeout(function () {
-          proxy.resume()
+          if (!proxy.destroyed && proxy.isPaused()) proxy.resume()
         }, 100)
       }
-    } catch (err) {
-    }
+    } catch (err) { }
+  })
+  proxy.on('drain', function () {
+    if (socket.isPaused()) socket.resume()
   })
   socket.on('data', function (d) {
-        // If the application tries to send data before the proxy is ready, then that is it's own problem.
+    // If the application tries to send data before the proxy is ready, then that is it's own problem.
     try {
-          // console.log('sending ' + d.length + ' bytes to proxy');
+      // console.log('sending ' + d.length + ' bytes to proxy');
       if (!proxy.write(d)) {
         socket.pause()
 
-        proxy.on('drain', function () {
-          socket.resume()
-        })
         setTimeout(function () {
-          socket.resume()
+          if (!socket.destroyed && socket.isPaused()) socket.resume()
         }, 100)
       }
     } catch (err) { }
   })
 
-  proxy.on('error', err => {}) // console.log('Ignore proxy error');
+  proxy.on('error', err => { }) // console.log('Ignore proxy error');
 
   proxy.on('close', had_error => {
     try {
@@ -61,7 +60,7 @@ const server = socks.createServer(function (socket, port, address, proxy_ready) 
       } else {
         console.error('Connect to %s:%d failed', address, port)
       }
-      socket.end()
+      if (!socket.destroyed) socket.end()
     } catch (err) { }
   })
 
@@ -70,10 +69,9 @@ const server = socks.createServer(function (socket, port, address, proxy_ready) 
     try {
       if (this.proxy !== undefined) {
         proxy.removeAllListeners('data')
-        proxy.end()
+        if (!proxy.destroyed) proxy.end()
       }
-          // console.error('The socket %s:%d closed',socket.remoteAddress,socket.remotePort);
-    } catch (err) { }
+    } catch (err) { } // console.error('The socket %s:%d closed',socket.remoteAddress,socket.remotePort);
   }.bind(this))
 }, process.env.N_T_PROXY_USER && process.env.N_T_PROXY_PASS && {
   username: process.env.N_T_PROXY_USER,
